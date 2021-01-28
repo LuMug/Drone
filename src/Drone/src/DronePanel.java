3e202fb5d5241cc85a0b4ffec77852d63102ed05
AlessandroAloise <54986411+AlessandroAloise@users.noreply.github.com>
2021-01-28 10:40:55 +0100

    Creazione del panel
    Creazione del panel voto.

---
+++ src/Drone/src/DronePanel.java
@@ -0,0 +1,35 @@
+/**
+ * Panel che contiene la grafiche dell'applicazione.
+ * 
+ * @author Alessandro Aloise
+ * @version 28.01.2021
+ */
+public class DronePanel extends javax.swing.JPanel {
+
+    /**
+     * Creates new form DronePanel
+     */
+    public DronePanel() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
