55de409f6dac65351f15b8a432dd853a820855e5
AlessandroAloise <54986411+AlessandroAloise@users.noreply.github.com>
2021-03-18 11:25:57 +0100

    classe che si occupa del log

---
+++ src/Drone/src/Log.java
@@ -0,0 +1,51 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Classe che si occupa di log.
+ *
+ * @author Alessandro Aloise
+ * @version 11.03.2021
+ */
+public class Log {
+
+    public static FileWriter fw;
+    public static File file;
+
+    public void creazioneFile() {
+        try {
+            Date data = new Date();
+            DateFormat dateFormat;
+            dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.ITALY);
+            String path = "log/Log_" + dateFormat.format(data).replace(' ', '_') + " .txt";
+            file = new File(path);
+            file.createNewFile();
+            fw = new FileWriter(file);
+        } catch (IOException ex) {
+            System.out.println("Error file già esistente");
+        }
+    }
+
+    public void scritturaFile(String testo) {
+        try {
+            fw.write(testo + '\n');
+            fw.flush();
+
+        } catch (IOException e) {
+            System.out.println("Error: stringa non valida");
+        }
+    }
+
+    public void chiusuraFile() {
+        try {
+            fw.close();
+        } catch (IOException ex) {
+            System.out.println("Error: stringa non valida");
+        }
+    }
+}
--- src/Drone/src/Status.java
+++ src/Drone/src/Status.java
@@ -4,77 +4,155 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Classe che stampa come output i valori del drone.
- * 
+ *
  * @version 11.03.2021
  * @author Alessandro Aloise
  */
 public class Status extends Thread {
 
-    public static void main(String[] args) throws SocketException, IOException {
+    Log log = new Log();
+
+    public String pich ="";
+    public String roll="";
+    public String yaw="";
+    public String spX="";
+    public String spY="";
+    public String spZ="";
+    public String templ="";
+    public double temMinF;
+    public double temMinC;
+    public String temph="";
+    public double temMaxF;
+    public double temMaxC;
+    public String tof="";
+    public String altezza="";
+    public String bat="";
+    public String baro="";
+    public String time="";
+    public String agx="";
+    public String agy="";
+    public String agz="";
+    public int port;
+    public String ip;
+    DateFormat dateFormat;
+    Date data = new Date();
+
+
+    public void run() {
+        dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.ITALY);
+        log.creazioneFile();
+        DatagramSocket socket;
+        byte[] buf = new byte[256];
         try {
-            DatagramSocket socket;
-            byte[] buf = new byte[256];
             socket = new DatagramSocket(8890);
             boolean fine = true;
 
             while (fine) {
                 DatagramPacket packet = new DatagramPacket(buf, buf.length);
                 socket.receive(packet);
-
                 InetAddress address = packet.getAddress();
                 int port = packet.getPort();
                 packet = new DatagramPacket(buf, buf.length, address, port);
                 String received = new String(packet.getData(), 0, packet.getLength());
                 socket.receive(packet);
-                // System.out.println("Drone: " + received);
 
                 StringTokenizer st = new StringTokenizer(received, " ;");
-                String pich = st.nextToken();
-                String roll = st.nextToken();
-                String yaw = st.nextToken().substring(4);
-                String spX = st.nextToken().substring(4);
-                String spY = st.nextToken().substring(4);
-                String spZ = st.nextToken().substring(4);
-                String templ = st.nextToken();
-                double temMinF = Integer.parseInt(templ.substring(6));
-                double temMinC = (temMinF - 32) * 0.5;
-                String temph = st.nextToken();
-                double temMaxF = Integer.parseInt(temph.substring(6));
-                double temMaxC = (temMaxF - 32) * 0.5;
-                String tof = st.nextToken();
-                String altezza = st.nextToken().substring(2);
-                String bat = st.nextToken().substring(4);
-                System.out.println("Valori del Drone:");
-                System.out.print("Batteria: ");
-                System.out.println(bat);
-                System.out.print("Temeratura minima: ");
-                System.out.println(temMinC);
-                System.out.print("Temeratura massima: ");
-                System.out.println(temMaxC);
-                System.out.print("Velocita x: ");
-                System.out.println(spX);
-                System.out.print("Velocita y: ");
-                System.out.println(spY);
-                System.out.print("Velocita Z: ");
-                System.out.println(spZ);
-                System.out.print("Altezza: ");
-                System.out.println(altezza);
-                System.out.print("Altitudine: ");
-                System.out.println(yaw);
+                pich = st.nextToken();
+                roll = st.nextToken();
+                yaw = st.nextToken().substring(4);
+                spX = st.nextToken().substring(4);
+                spY = st.nextToken().substring(4);
+                spZ = st.nextToken().substring(4);
+                templ = st.nextToken();
+                temMinF = Integer.parseInt(templ.substring(6));
+                temMinC = (temMinF - 32) * 0.5;
+                temph = st.nextToken();
+                temMaxF = Integer.parseInt(temph.substring(6));
+                temMaxC = (temMaxF - 32) * 0.5;
+                tof = st.nextToken();
+                altezza = st.nextToken().substring(2);
+                bat = st.nextToken().substring(4);
+                baro = st.nextToken().substring(5);
+                time = st.nextToken().substring(5);
+                agx = st.nextToken().substring(4);
+                agy = st.nextToken().substring(4);
+                agz = st.nextToken().substring(4);
+
                 Thread.sleep(1000);
-                System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+                String valori = " Bat:" + bat
+                        + " TMax:" + temMaxC
+                        + " Vx:" + spX
+                        + " Vy:" + spY
+                        + " Vz:" + spZ
+                        + " h:" + altezza
+                        + " Ax:" + agx
+                        + " Ay:" + agy
+                        + " Az:" + agz
+                        + " TCm: " + time;
+                String finale= dateFormat.format(data) + " "+ ip + ":"+ port + valori;
+                log.scritturaFile(finale);
+
                 if (temMinC == 100) {
                     fine = false;
                     socket.close();
+                    log.chiusuraFile();
                 }
             }
-
-        } catch (InterruptedException ex) {
-            System.out.println("Error");
+        } catch (SocketException ex) {
+            Logger.getLogger(Status.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException | InterruptedException ex) {
+            Logger.getLogger(Status.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    public String getValori() {
+        String valori = "Batteria:" + bat
+                + " TemeraturaMax:" + temMaxC
+                + " VelocitaX:" + spX
+                + " VelocitaY:" + spY
+                + " VelocitaZ:" + spZ
+                + " Altezza:" + altezza
+                + " AccelerazioneX:" + agx
+                + " AccelerazioneY:" + agy
+                + " AccelerazioneZ:" + agz
+                + "TempoCm" + time;
+        return valori;
+    }
+
+    public void stampa() {
+        System.out.println("Valori del Drone:");
+        System.out.print("Batteria: ");
+        System.out.println(bat);
+        System.out.print("Temeratura minima: ");
+        System.out.println(temMinC);
+        System.out.print("Temeratura massima: ");
+        System.out.println(temMaxC);
+        System.out.print("Velocita x: ");
+        System.out.println(spX);
+        System.out.print("Velocita y: ");
+        System.out.println(spY);
+        System.out.print("Velocita Z: ");
+        System.out.println(spZ);
+        System.out.print("Altezza: ");
+        System.out.println(altezza);
+        System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+    }
+    
+    public void setIp(String ip){
+        this.ip=ip;
+    }
+
+    void setport(int porta) {
+        this.port=porta;
+    }
+
 }
