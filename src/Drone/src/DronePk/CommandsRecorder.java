0f109ac0fe1116319ef669e125f428d527c89cbf
SamueleGanci <samuele.ganci@samtrevano.ch>
2021-04-29 11:17:21 +0200

    Undo fix codice

--- src/Drone/src/DronePk/CommandsRecorder.java
+++ src/Drone/src/DronePk/CommandsRecorder.java
@@ -24,11 +24,6 @@ public class CommandsRecorder {
      * Variabile per la Path file.
      */
     private Path file;
-    
-    /**
-     * Variabile di controllo.
-     */
-    private boolean check = false;
 
     /**
      * Metodo che si occupa di registrare la sequenza di comandi.
@@ -50,12 +45,7 @@ public class CommandsRecorder {
      */
     public void sequenceWriter(String sequence) {
         try {
-            if(sequence.equals("takeoff")) {
-                check = true;
-            }
-            if(check) {
-                Files.write(file, ((sequence + "\r\n")).getBytes(), StandardOpenOption.APPEND);
-            }
+            Files.write(file, ((sequence + "\r\n")).getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
         }
     }
