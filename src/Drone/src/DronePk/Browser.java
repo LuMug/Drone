23c61bcfb3785e5e16aa87b7a577fb7a757e5bbd
AlessandroAloise <54986411+AlessandroAloise@users.noreply.github.com>
2021-05-06 11:31:46 +0200

    aggiunto metodo per l'esecuzione
    aggiunto il metodo per l'esecuzione della live

--- src/Drone/src/DronePk/Browser.java
+++ src/Drone/src/DronePk/Browser.java
@@ -6,28 +6,41 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- * Classe che si occupa di aprire a schermo una schermata del browser predefinito.
+ * Classe che si occupa di aprire a schermo una schermata del browser
+ * predefinito.
+ *
  * @author Alessandro Aloise
  * @version 06.05.2021
  */
 public class Browser {
 
-    public void openBrowser(){
-        String url = "http://localhost:3000";
-        
+    public void script() throws IOException {
+        String os= System.getProperty("os.name").toLowerCase();
+        if (os.contains("os")) {
+            System.out.println("mac");
+        } else {
+            String path = "cmd /c start RunLiveWin.bat";
+            Runtime rn = Runtime.getRuntime();
+            Process pr = rn.exec(path);
+        }
+    }
+
+    public void openBrowser() {
+        String url = "http://localhost:3000/index.html";
+
         if (Desktop.isDesktopSupported()) {
             Desktop desktop = Desktop.getDesktop();
             try {
                 desktop.browse(new URI(url));
             } catch (IOException | URISyntaxException e) {
-                System.out.println("Error:" +e );
+                System.out.println("Error:" + e);
             }
-        }else{
+        } else {
             Runtime runtime = Runtime.getRuntime();
             try {
                 runtime.exec("xdg-open " + url);
             } catch (IOException e) {
-                System.out.println("Error:" +e);
+                System.out.println("Error:" + e);
             }
         }
     }
